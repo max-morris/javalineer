ad5d16d85f48e20e09623011c16fe4d8c8f40a0c
Max Morris <admin@maxmorris.com>
2025-03-22 15:26:39 -0500

    Implemented Iterable for ListView, and added unchecked versions of get and set methods.

--- src/main/java/edu/lsu/cct/javalineer/ListView.java
+++ src/main/java/edu/lsu/cct/javalineer/ListView.java
@@ -1,8 +1,9 @@
 package edu.lsu.cct.javalineer;
 
+import java.util.Iterator;
 import java.util.List;
 
-public class ListView<E> {
+public class ListView<E> implements Iterable<E> {
     private final List<E> underlying;
     private final int offset, size;
 
@@ -12,21 +13,46 @@ public class ListView<E> {
         this.size = size;
     }
 
+    public E getUnchecked(int i) {
+        return underlying.get(offset + i);
+    }
+
+    public void setUnchecked(int i, E e) {
+        underlying.set(offset + i, e);
+    }
+
     public E get(int i) {
         if (i < 0 || i >= size) {
             throw new IndexOutOfBoundsException(i);
         }
-        return underlying.get(offset + i);
+        return getUnchecked(i);
     }
 
     public void set(int i, E e) {
         if (i < 0 || i >= size) {
             throw new IndexOutOfBoundsException(i);
         }
-        underlying.set(offset + i, e);
+        setUnchecked(i, e);
     }
 
     public int size() {
         return size;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int idx = 0;
+
+            @Override
+            public boolean hasNext() {
+                return idx < size;
+            }
+
+            @Override
+            public E next() {
+                return getUnchecked(idx++);
+            }
+        };
     }
 }
